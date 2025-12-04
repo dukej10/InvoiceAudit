cdb3fe4df4f48d41f11ab82bb81700ec01bbfaa3
Juan Diego Duque <diegoduqqe@gmail.com>
2025-12-03 20:48:27 -0500

    feat: add exception management

---
+++ ms_invoice_management/infrastructure/entry-points/reactive-web/src/main/java/co/com/management/api/exception/MissingParametersException.java
@@ -0,0 +1,16 @@
+package co.com.management.api.exception;
+
+import java.util.Set;
+
+public class MissingParametersException extends RuntimeException {
+    private final Set<String> missingParameters;
+
+    public MissingParametersException(Set<String> missing) {
+        super("Faltan parámetros obligatorios");
+        this.missingParameters = Set.copyOf(missing);
+    }
+
+    public Set<String> getMissingParameters() {
+        return missingParameters;
+    }
+}
