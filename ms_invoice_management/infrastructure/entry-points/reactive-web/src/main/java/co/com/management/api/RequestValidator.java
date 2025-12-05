1a7bad83ed9d3409aac59f62dc00b4495af8eb85
Juan Diego Duque <diegoduqqe@gmail.com>
2025-12-04 20:15:59 -0500

    feat: add path vble validator

--- ms_invoice_management/infrastructure/entry-points/reactive-web/src/main/java/co/com/management/api/RequestValidator.java
+++ ms_invoice_management/infrastructure/entry-points/reactive-web/src/main/java/co/com/management/api/RequestValidator.java
@@ -24,4 +24,33 @@ public class RequestValidator {
                 ? Mono.just(request)
                 : Mono.error(new MissingParametersException(missing));
     }
+
+    public Mono<ServerRequest> requirePathVariables(
+            ServerRequest request,
+            Collection<String> requiredPathVars) {
+
+        Set<String> missing = requiredPathVars.stream()
+                .filter(v -> {
+                    try {
+                        request.pathVariable(v);
+                        return false; // existe
+                    } catch (IllegalArgumentException ex) {
+                        return true; // no existe
+                    }
+                })
+                .collect(Collectors.toSet());
+
+        return missing.isEmpty()
+                ? Mono.just(request)
+                : Mono.error(new MissingParametersException(missing));
+    }
+
+    public Mono<ServerRequest> requireParamsAndPathVars(
+            ServerRequest request,
+            Collection<String> requiredParams,
+            Collection<String> requiredPathVars) {
+
+        return requireParams(request, requiredParams)
+                .flatMap(req -> requirePathVariables(req, requiredPathVars));
+    }
 }
