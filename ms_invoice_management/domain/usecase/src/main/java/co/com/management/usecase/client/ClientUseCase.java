d11d99135e41318a608a489f6735ed806dbe4e98
Juan Diego Duque <diegoduqqe@gmail.com>
2025-12-06 21:24:26 -0500

    feat: correct delete flow

--- ms_invoice_management/domain/usecase/src/main/java/co/com/management/usecase/client/ClientUseCase.java
+++ ms_invoice_management/domain/usecase/src/main/java/co/com/management/usecase/client/ClientUseCase.java
@@ -3,16 +3,21 @@ package co.com.management.usecase.client;
 import co.com.management.model.PageResult;
 import co.com.management.model.client.Client;
 import co.com.management.model.client.gateways.ClientRepository;
+import co.com.management.model.invoice.gateways.InvoiceRepository;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 public class ClientUseCase {
     private final ClientRepository clientRepository;
 
+    private final InvoiceRepository invoiceRepository;
+
     public Mono<Client> saveClient(Client client) {
+        client.setCreatedDate(LocalDateTime.now());
         return clientRepository.findByDocumentNumberAndDocumentType(client.getDocumentNumber(),
                 client.getDocumentType()).switchIfEmpty(
                      clientRepository.saveClient(client)
@@ -20,6 +25,7 @@ public class ClientUseCase {
     }
 
     public Mono<Client> updateClient(Client client) {
+        client.setUpdatedDate(LocalDateTime.now());
         return clientRepository.findById(client.getId())
                 .flatMap(clientFound -> clientRepository.saveClient(client));
     }
@@ -29,7 +35,9 @@ public class ClientUseCase {
     }
 
     public Mono<Void> deleteById(UUID id) {
-        return clientRepository.deleteClient(id);
+        return invoiceRepository.deleteAllByClientId(id).then(
+                clientRepository.deleteClient(id)
+        );
     }
 
     public Mono<PageResult<Client>> getAllPageable(int page, int size) {
