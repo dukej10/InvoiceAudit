c2ddb92d5a5b50256c2e21bd864963bfbc42f59f
Juan Diego <diegoduqqe@gmail.com>
2026-01-19 21:33:32 -0500

    feat: solved error connection

--- ms_credit_management/infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/credit/dynamodb/config/DynamoDBConfig.java
+++ ms_credit_management/infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/credit/dynamodb/config/DynamoDBConfig.java
@@ -3,14 +3,17 @@ package co.com.credit.dynamodb.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 
 import java.net.URI;
+import java.time.Duration;
 
 @Configuration
 public class DynamoDBConfig {
@@ -19,35 +22,36 @@ public class DynamoDBConfig {
     @Profile("local")
     public DynamoDbAsyncClient dynamoDbAsyncClientLocal(
             @Value("${aws.dynamodb.endpoint}") String endpoint,
-            @Value("${aws.region}") String region) {
+            @Value("${aws.region}") String region,
+            @Value("${aws.dynamodb.access-key-id}") String accessKey,
+            @Value("${aws.dynamodb.secret-access-key}") String secretKey) {
 
         return DynamoDbAsyncClient.builder()
                 .endpointOverride(URI.create(endpoint))
                 .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
+                .overrideConfiguration(
+                        ClientOverrideConfiguration.builder()
+                                .apiCallTimeout(Duration.ofSeconds(5))
+                                .apiCallAttemptTimeout(Duration.ofSeconds(3))
+                                .build()
+                )
                 .build();
     }
 
     @Bean
     @Profile({"dev", "cer", "pdn"})
-    public DynamoDbAsyncClient dynamoDbAsyncClientCloud(@Value("${aws.region}") String region) {
+    public DynamoDbAsyncClient dynamoDbAsyncClientCloud(
+            @Value("${aws.region}") String region) {
+
         return DynamoDbAsyncClient.builder()
                 .credentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
                 .region(Region.of(region))
                 .build();
-    }
-
-    @Bean
-    @Primary
-    public DynamoDbAsyncClient dynamoDbAsyncClient(
-            @Value("${aws.dynamodb.endpoint:}") String endpoint) {
-
-        if (!endpoint.isBlank()) {
-            // Perfil local
-            return dynamoDbAsyncClientLocal(endpoint, "us-east-1");
-        } else {
-            // Perfiles cloud
-            return dynamoDbAsyncClientCloud("us-east-1");
-        }
     }
 
     @Bean
